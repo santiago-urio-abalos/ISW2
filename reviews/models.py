fef754228d4aec816a5de7c0475b0b9f626a8cf4
santiago-urio-abalos <9301368@alumnos.ufv.es>
2025-12-12 21:32:41 +0100

    feat:develop

--- reviews/models.py
+++ reviews/models.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-# reviews/models.py
-from django.db import models
-from django.contrib.auth.models import User
-from relecloud.models import Destination
-
-class Review(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.TextField()
-    rating = models.PositiveSmallIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, default=1)
-
-
-    def __str__(self):
-        return f"{self.author} - {self.destination} ({self.rating})"
-=======
 # reviews/models.py
 from django.db import models
 from django.contrib.auth.models import User
@@ -33,4 +16,3 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.author} - {self.destination} ({self.rating})"
->>>>>>> 089ae2fcb446be135d0a687d33e6a8eeefc8a59d
