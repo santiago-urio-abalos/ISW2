4d4942163306380134cf9178bf980125831d6cc3
santiago-urio-abalos <9301368@alumnos.ufv.es>
2025-12-12 19:19:35 +0100

    New commit message

--- reviews/apps.py
+++ reviews/apps.py
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 96c4a1d8085dc55cd4a61d72cf3c47c6915c8b8c
+
 from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
-<<<<<<< HEAD
-=======
 from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
->>>>>>> 089ae2fcb446be135d0a687d33e6a8eeefc8a59d
-=======
->>>>>>> 96c4a1d8085dc55cd4a61d72cf3c47c6915c8b8c
