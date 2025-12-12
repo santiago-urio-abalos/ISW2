dadcb0bd6968bfae9d958c229a68cd2458c49a09
miguelangel-gomez-gonzalez <9305657@alumnos.ufv.es>
2025-12-12 11:09:37 +0100

    cambios en settings.py

--- proyect/settings.py
+++ proyect/settings.py
@@ -4,8 +4,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_USER = 'miguigomez11@gmail.com'  # Pon aquí tu correo real
+EMAIL_HOST_PASSWORD = 'UFV1234'  # Contraseña de prueba proporcionada
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 """
 Django settings for proyect project.
