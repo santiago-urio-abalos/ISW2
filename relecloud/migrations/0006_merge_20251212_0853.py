3b87c15f672b7156824d89baee2d32099aa873c4
santiago-urio-abalos <9301368@alumnos.ufv.es>
2025-12-12 15:08:52 +0100

    feat: Reviews, modifying a few things

--- relecloud/migrations/0006_merge_20251212_0853.py
+++ relecloud/migrations/0006_merge_20251212_0853.py
@@ -6,8 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('relecloud', '0004_review'),
         ('relecloud', '0005_remove_purchase_cruise_alter_purchase_destination'),
+        ('relecloud', '0006_review'),
     ]
 
     operations = [
