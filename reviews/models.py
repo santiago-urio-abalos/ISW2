bc8f57ee38b2f64cb3447bff3b108499e6d2f1fd
santiago-urio-abalos <150132817+santiago-urio-abalos@users.noreply.github.com>
2025-12-12 19:22:13 +0100

    Resolve merge conflict in reviews/models.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 96c4a1d8085dc55cd4a61d72cf3c47c6915c8b8c
+
 # reviews/models.py
 from django.db import models
 from django.contrib.auth.models import User
@@ -20,8 +17,7 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.author} - {self.destination} ({self.rating})"
-<<<<<<< HEAD
-=======
+
 # reviews/models.py
 from django.db import models
 from django.contrib.auth.models import User
@@ -39,7 +35,3 @@ class Review(models.Model):
     )
 
     def __str__(self):
-        return f"{self.author} - {self.destination} ({self.rating})"
->>>>>>> 089ae2fcb446be135d0a687d33e6a8eeefc8a59d
-=======
->>>>>>> 96c4a1d8085dc55cd4a61d72cf3c47c6915c8b8c
